3a3d45e7a7f5ced24d05ee215879fe157d5baabe
Hiroaki Nakamura <hnakamur@gmail.com>
2012-11-16 07:19:10 +0900

    Rename 'json' directory to 'hosts'

--- solo.rb
+++ solo.rb
@@ -1,6 +1,6 @@
 file_cache_path '/tmp/chef-solo'
 cookbook_path   '/etc/chef/cookbooks'
-json_attribs    '/etc/chef/json//%s.json' % `hostname -s`.chomp
+json_attribs    '/etc/chef/hosts//%s.json' % `hostname -s`.chomp
 role_path       '/etc/chef/roles'
 node_name       `hostname -s`.chomp
 log_level       :debug
