4a5eef7416d0119cd27ab237f2e4c8ca8e57e773
Hiroaki Nakamura <hnakamur@gmail.com>
2012-11-16 02:03:00 +0900

    Fix chef_dir to /etc/chef and stop generating solo.rb dynamically.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,25 @@
 require "json"
 
 set :application, "chef-solo"
-set :chef_dir,    "/etc/chef"
 set :hostname,    `hostname -s`.chomp
 
 namespace :chef do
 
   task :default do
-    init_config
     sync
     run_chef
   end
 
-  task :init_config do
-    File::open("#{chef_dir}/solo.rb", "w") {|f|
-      f.write <<-EOF
-file_cache_path '/tmp/chef-solo'
-cookbook_path   '#{chef_dir}/cookbooks'
-json_attribs    '#{chef_dir}/json//%s.json' % `hostname -s`.chomp
-role_path       '#{chef_dir}/roles'
-node_name       `hostname -s`.chomp
-log_level       :debug
-      EOF
-    }
-  end
-
   desc "run chef-solo"
   task :run_chef, :roles => :host do
-    run "chef-solo -c #{chef_dir}/solo.rb"
+    run "chef-solo"
   end
 
-  desc "rsync #{chef_dir}"
+  desc "rsync /etc/chef"
   task :sync do
     find_servers_for_task(current_task).each do |server|
       next if server.host == hostname
-      `rsync -avC --delete -e ssh #{chef_dir}/ #{server.host}:#{chef_dir}/`
+      `rsync -avC --delete -e ssh /etc/chef/ #{server.host}:/etc/chef/`
     end
   end
 
